03d928f8d9f708491c941bd7ec885a9410e9bcfe
Kenny Younger <kyounger@gmail.com>
2021-03-12 10:40:59 -0600

    Use a better caching strategy
    This will enable further parameterizing, especially of CI_VERSION

--- run.sh
+++ run.sh
@@ -1,40 +1,51 @@
 set -euo pipefail
 
+#adjustable vars
 CI_VERSION="2.263.4.2"
 CB_UPDATE_CENTER="https://jenkins-updates.cloudbees.com/update-center/envelope-core-mm"
 CB_DOCKER_IMAGE="cloudbees/cloudbees-core-mm"
-# CB_UPDATE_CENTER="https://jenkins-updates.cloudbees.com/update-center/envelope-core-oc"
-# CB_DOCKER_IMAGE="cloudbees/cloudbees-cloud-core-oc"
 
+#calculated vars
 PLUGIN_YAML_PATH=${1:="plugin.yaml"}
+CB_UPDATE_CENTER_URL="$CB_UPDATE_CENTER/update-center.json?version=$CI_VERSION"
+
+#cache some stuff locally
+CACHE_BASE_DIR=$(pwd)/.cache
+mkdir -p $CACHE_BASE_DIR
 
 #reformat plugins list to be compatible with pimt
 yq e '.plugins[] | [{"artifactId": .[]}] | {"plugins": .}' $PLUGIN_YAML_PATH > pimt-plugins.yaml
 
 #use docker to extract war file from cb image and cache it
-if [[ -f jenkins.war ]]; then
-  echo "jenkins.war already exist in the pwd, remove it if you need to refresh" >&2
+WAR_CACHE_DIR=$CACHE_BASE_DIR/war/$CI_VERSION
+if [[ -f $WAR_CACHE_DIR/jenkins.war ]]; then
+  echo "$WAR_CACHE_DIR/jenkins.war already exist, remove it if you need to refresh" >&2
 else
-  docker run -ti -v $(pwd):/war --user root --entrypoint "" $CB_DOCKER_IMAGE:$CI_VERSION cp /usr/share/jenkins/jenkins.war /war
+  mkdir -p $WAR_CACHE_DIR
+  docker run -ti -v $WAR_CACHE_DIR:/war --user root --entrypoint "" $CB_DOCKER_IMAGE:$CI_VERSION cp /usr/share/jenkins/jenkins.war /war/jenkins.war
 fi
 
 #cache PIMT
-if [[ -f jenkins-plugin-manager.jar ]]; then
-  echo "jenkins-plugin-manager.jar already exist in the pwd, remove it if you need to refresh" >&2
+PIMT_CACHE_DIR=$CACHE_BASE_DIR/pimt
+if [[ -f $PIMT_CACHE_DIR/jenkins-plugin-manager.jar ]]; then
+  echo "$PIMT_CACHE_DIR/jenkins-plugin-manager.jar already exist, remove it if you need to refresh" >&2
 else
+  mkdir -p $PIMT_CACHE_DIR
   JAR_URL=$(curl -sL \
     -H "Accept: application/vnd.github.v3+json" \
     https://api.github.com/repos/jenkinsci/plugin-installation-manager-tool/releases/latest \
     | yq e '.assets[0].browser_download_url' -)
 
-  curl -sL $JAR_URL > jenkins-plugin-manager.jar 
+  curl -sL $JAR_URL > $PIMT_CACHE_DIR/jenkins-plugin-manager.jar 
 fi
 
 #cache the UC locally (we use update-center.json as the file because that's what PIMT expects)
-if [[ -f update-center.json ]]; then
-  echo "update-center.json already exist in the pwd, remove it if you need to refresh" >&2
+UC_CACHE_DIR=$CACHE_BASE_DIR/uc/$CI_VERSION
+if [[ -f $UC_CACHE_DIR/update-center.json ]]; then
+  echo "$UC_CACHE_DIR/update-center.json already exist, remove it if you need to refresh" >&2
 else
-  curl -sL "$CB_UPDATE_CENTER/update-center.json?version=$CI_VERSION" > update-center.json
+  mkdir -p $UC_CACHE_DIR
+  curl -sL "$CB_UPDATE_CENTER_URL" > $UC_CACHE_DIR/update-center.json
 fi
 
 #run PIMT and reformat output to get the plugin-catalog.yaml
@@ -46,11 +57,11 @@ displayName: Rbac with casc for preview
 configurations:
 - description: these are tier3 plugins
   includePlugins:"
-java -jar jenkins-plugin-manager.jar \
-  --war jenkins.war \
+java -jar $PIMT_CACHE_DIR/jenkins-plugin-manager.jar \
+  --war $WAR_CACHE_DIR/jenkins.war \
   --list \
   --no-download \
-  --jenkins-update-center "file://$(pwd)" \
+  --jenkins-update-center "file://$UC_CACHE_DIR" \
   --plugin-file pimt-plugins.yaml \
   | sed -n '/^Plugins\ that\ will\ be\ downloaded\:$/,/^Resulting\ plugin\ list\:$/p' \
   | sed '1d' | sed '$d' | sed '$d' \
