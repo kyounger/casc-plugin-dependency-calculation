98e816d1c1e8d4c12a20f52199cf07ca579212ec
sboardwell <sboardwell@cloudbees.com>
2023-08-03 08:58:15 +0200

    Add download test

---
+++ tests/simple-download/command.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd "$(cd "$(dirname "$0")" && pwd)"
+
+"${RUN_CMD}" \
+    -v '2.387.3.5' \
+    -t mm \
+    -f "source-plugins.yaml" \
+    -F "actual-plugins.yaml" \
+    -c "actual-plugin-catalog.yaml" \
+    -C "actual-plugin-catalog-offline.yaml" \
+    -s \
+    -G "actual-plugins-minimal.yaml" \
+    -d \
+    -D 'http://plugin-catalog/plugins/PNAME/PVERSION/PNAME.hpi'
