e813b035f6e9628f715ca34fe412cc30d8e565bb
sboardwell <sboardwell@cloudbees.com>
2023-08-29 12:34:19 +0200

    Add annotations tests

---
+++ tests/simple-annotations-override/command.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd "$(cd "$(dirname "$0")" && pwd)"
+
+"${RUN_CMD}" \
+    -v '2.387.3.5' \
+    -t mm \
+    -f "source-plugins.yaml" \
+    -f "source-plugins-v2.yaml" \
+    -F "actual/plugins.yaml" \
+    -c "actual/plugin-catalog.yaml" \
+    -C "actual/plugin-catalog-offline.yaml" \
+    -s \
+    -M \
+    -g "actual/plugins-minimal-for-generation-only.yaml" \
+    -G "actual/plugins-minimal.yaml"
---
+++ tests/simple-annotations/command.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd "$(cd "$(dirname "$0")" && pwd)"
+
+"${RUN_CMD}" \
+    -v '2.387.3.5' \
+    -t mm \
+    -f "source-plugins.yaml" \
+    -F "actual/plugins.yaml" \
+    -c "actual/plugin-catalog.yaml" \
+    -C "actual/plugin-catalog-offline.yaml" \
+    -s \
+    -g "actual/plugins-minimal-for-generation-only.yaml" \
+    -G "actual/plugins-minimal.yaml"
